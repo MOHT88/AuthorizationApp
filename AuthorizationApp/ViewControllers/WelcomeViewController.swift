09048e68f3b9679e0ddc368663b3eb6818e3a7cc
Sergey Ivanov <regspam88@ya.ru>
2022-03-12 15:20:26 +0300

    edit background color

--- AuthorizationApp/ViewControllers/WelcomeViewController.swift
+++ AuthorizationApp/ViewControllers/WelcomeViewController.swift
@@ -13,14 +13,33 @@ class WelcomeViewController: UIViewController {
     
     var user = ""
     
+    private let primaryColor = UIColor(
+        red: 210/255,
+        green: 109/255,
+        blue: 128/255,
+        alpha: 1
+    )
+    private let secondaryColor = UIColor(
+        red: 107/255,
+        green: 148/255,
+        blue: 230/255,
+        alpha: 1
+    )
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        let layer = CAGradientLayer()
-        layer.frame = view.bounds
-        layer.colors = [UIColor.systemBlue.cgColor, UIColor.systemRed.cgColor]
-        view.layer.insertSublayer(layer, at: 0)
-       
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
         welcomeTextLabel.text = "Welcome, \(user)!"
+    }
+}
+// MARK: - Set background color
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
     }
 }
